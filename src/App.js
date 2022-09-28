55c9b34d7226974ce23ab14d6d9a75d6ca1152e7
Dhara Solanki <83348790+Dhara3078@users.noreply.github.com>
2022-09-28 15:20:02 +0530

    added login

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import Home from "./views/Home";
 
 
@@ -6,6 +7,25 @@ function App() {
     <main className="bg-primary-900 min-h-auto font-ubuntu">
       <Home/>
     </main>
+=======
+// import logo from './logo.svg';
+// import './App.css';
+import React from "react";
+import Nav from "./components/Nav";
+import Login from "./components/Login";
+import { Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <React.Fragment>
+      <main className="bg-primary-900 h-screen">
+        <Nav/>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </main>
+    </React.Fragment>
+>>>>>>> Stashed changes
   );
 }
 
