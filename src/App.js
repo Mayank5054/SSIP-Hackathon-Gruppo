aa5020b7e982fec81fb74686fbbcfd940b5399e3
Dhara Solanki <83348790+Dhara3078@users.noreply.github.com>
2022-10-05 18:40:04 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Home from "./views/Home";
 import { Routes, Route, useNavigate } from "react-router-dom";
 import LoginPage from "./views/LoginPage";
@@ -6,12 +6,28 @@ import RegisterPage from "./views/RegisterPage";
 import Meets from "./views/Meets";
 import pathContext from "./context/path-context";
 import MeetDetails from "./views/MeetDetails";
+import CalendarPage from "./views/calendarPage";
+import axios from "axios";
+
 import CreateNewMeet from "./views/CreateNewMeet";
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
 import {AdapterDateFns} from "@mui/x-date-pickers/AdapterDateFns";
 
 
 function App() {
+    useEffect(() => {
+        var key = process.env.REACT_APP_API_KEY;
+        console.log(key);
+        axios
+            .get(
+                "http://dataservice.accuweather.com/locations/v1/cities/search?apikey=" +
+                    key +
+                    "&q=Surat"
+            )
+            .then((e) => {
+                console.log(e);
+            });
+    }, []);
     const navigate = useNavigate();
     const navigateToRegister = () => {
         navigate("/register");
@@ -29,8 +45,10 @@ function App() {
         navigate("/createnewmeet");
     };
     const navigateToMeetsDetail = (id) => {
-        console.log("id: ", id, typeof id, "from navigate");
         navigate(`/meetdetails/:${id}`);
+    };
+    const navigateToCalendar = () => {
+        navigate("/calendarpage");
     };
     return (
         <LocalizationProvider dateAdapter={AdapterDateFns}>
@@ -41,7 +59,11 @@ function App() {
                 navigateToHome: navigateToHome,
                 navigateToMeets: navigateToMeets,
                 navigateToMeetsDetail: navigateToMeetsDetail,
+<<<<<<< HEAD
                 navigateToCreateNewMeet: navigateToCreateNewMeet,
+=======
+                navigateToCalendar: navigateToCalendar,
+>>>>>>> 030cab6a55ff2bdf69c6b686baf4be1101af8648
             }}>
             <Routes>
                 <Route path='/register' element={<RegisterPage />} />
@@ -49,7 +71,11 @@ function App() {
                 <Route path='/' element={<Home />} />
                 <Route path='/meets' element={<Meets />} />
                 <Route path='/meetdetails/:meetid' element={<MeetDetails />} />
+<<<<<<< HEAD
                 <Route path='/createnewmeet' element={<CreateNewMeet />} />
+=======
+                <Route path='/calendarpage' element={<CalendarPage />} />
+>>>>>>> 030cab6a55ff2bdf69c6b686baf4be1101af8648
             </Routes>
         </pathContext.Provider>
         </LocalizationProvider>
