feec827b6e7ae5ac50516e0614cfb318f6112b08
Mayank <mayanksheladiya49@gmail.com>
2022-10-06 13:04:21 +0530

    google calendar API is integrated

--- src/App.js
+++ src/App.js
@@ -12,9 +12,44 @@ import axios from "axios";
 import CreateNewMeet from "./views/CreateNewMeet";
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
 import {AdapterDateFns} from "@mui/x-date-pickers/AdapterDateFns";
-
+import {gapi} from 'gapi-script';
 
 function App() {
+      var gapi=window.gapi;
+      console.log(gapi);
+  var CLIENT_ID="934057497734-2k0sp365v94u0u08ta8mv9b4qodkoal6.apps.googleusercontent.com";
+  var API_KEY="AIzaSyBI57Lt2FHhIiXvRZunhEvuqZhmgZ0lHbk";
+  const DISCOVERY_DOC = 'https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest';
+  const SCOPES = 'https://www.googleapis.com/auth/calendar.events';
+    const handleCal=()=>{
+  gapi.load('client:auth2',()=>{
+    console.log("client loaded");
+    gapi.client.init({
+        apiKey:API_KEY,
+        clientId:CLIENT_ID,
+        discoveryDocs:DISCOVERY_DOC,
+        scope:"profile email",
+        plugin_name:'Grouppo'
+        });
+        gapi.client.load('calendar','v3',()=>{
+          console.log("bam!");
+        })
+        gapi.auth2.getAuthInstance().signIn().then((e)=>{
+            console.log("logged in");console.log(e);
+            var data={
+                "summary":"hello world",
+                "start":{"date":"2022-10-07"},
+                "end":{"date":"2022-10-11"}
+            }
+            var req=gapi.client.calendar.events.insert({
+                "calendarId":"primary",
+                "resource":data
+            })
+            req.execute((e)=>{console.log("request sent");console.log(e);})
+        });
+  })
+ 
+    }
     useEffect(() => {
         var key = process.env.REACT_APP_API_KEY;
         console.log(key);
@@ -52,6 +87,7 @@ function App() {
     };
     return (
         <LocalizationProvider dateAdapter={AdapterDateFns}>
+          
             <pathContext.Provider
             value={{
                 navigateToRegister: navigateToRegister,
@@ -72,7 +108,9 @@ function App() {
                 <Route path='/calendarpage' element={<CalendarPage />} />
             </Routes>
         </pathContext.Provider>
+        <button onClick={handleCal}>click to authorized calendar</button>
         </LocalizationProvider>
+        
         
     );
 }
